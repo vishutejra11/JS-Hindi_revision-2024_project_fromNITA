073f4acb8e0cdf190acaf400a80a4ac2faf95a33
Anil Kumar Patidar <98152428+vishutejra11@users.noreply.github.com>
2023-10-14 11:23:27 +0000

    Comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,26 @@
+// Boolean Comparision -- true false 
+// console.log(2<1);
+// console.log(2>1);
+// console.log(2==1);
+// console.log(2>=1);
+// console.log(2<=1);
+
+// console.log("2"<1); // false-- it is not a good coversion -- Js inconsistency
+// console.log("02"<1);// false  -- it is not a good coversion -- Js inconsistency
+
+// console.log(null>0); // false
+// console.log(null>0); // false// for these cond comaparision treated null as 0
+// console.log(null>=0);// true 
+
+// console.log(undefined>0);// false 
+// console.log(undefined<0);// false 
+// console.log(undefined>=0);// false 
+
+// ===   ---> // false -- because === compares value with data type 
+console.log("2"===2); // false -- because === compares value with data type 
+
+
+
+
+
+

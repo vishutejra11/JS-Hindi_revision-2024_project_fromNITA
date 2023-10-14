4274984398e40c66d561b7ef6b9ec96bf623c8fd
Anil Kumar Patidar <98152428+vishutejra11@users.noreply.github.com>
2023-10-14 08:58:44 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict";// treat all JS code as new version 
+
+// alert(3+3) // we are using nodeJs , not browser
+
+console.log(3
+    +
+     3) // code redability should be high 
+
+
+console.log("Hitesh") 
+
+// console.log(3+3) console.log("Hitesh") -->error because there is same line code no ; redability low 
+
+let state ; // will be undefined 
+// DataTypes -- Primitive
+/*
+1.Number -- 2 to power 53 
+2.bigInt -- for storing big numbers
+3.string -- "Ak" , "" , we can use single code -- 'Anil'
+4.boolean -- true/false 
+5.null -- standalone value 
+6.undefined ==> undefined value 
+7.symbol -- unique 
+*/
+
+// Non Primitive Data types
+// Object -- user defined 
+
+console.log(typeof "Hitesh"); // string
+console.log(typeof undefined ); // undefined 
+console.log(typeof null); // Object -- it is a bug in js 
+
